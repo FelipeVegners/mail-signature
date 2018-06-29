7357167493689a6956917f349718ffa75291414f
Felipe Vegners <felipevegners@gmail.com>
2018-06-29 13:47:06 -0300

    HTML and SASS structured

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,3 +1,24 @@
+// Delete social field
+
+var socialField = document.querySelectorAll('.social-link');
+
+socialField.forEach(function (el) {
+    var closeBtn = el.querySelectorAll(".delete-link")[0];
+
+    el.addEventListener('mouseover', function () {
+        closeBtn.style.opacity = 1;
+    });
+    el.addEventListener('mouseout', function () {
+        closeBtn.style.opacity = 0;
+    });
+
+    closeBtn.addEventListener("click", function () {
+        this.parentElement.parentElement.style.display = 'none';
+        console.log('delete');
+    })
+});
+
+
 $("#font-size, #image-size, #image-shape").slider();
 
 let sourceCode = document.getElementById('signature-preview').outerHTML;
